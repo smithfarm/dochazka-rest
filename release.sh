727dc948e6310470a51176f0cf239bbaeb0fecc2
Nathan Cutler <ncutler@suse.com>
2015-07-21 15:42:05 +0200

    release.sh: add "osc update"
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ OBS_DIR="$HOME/home:smithfarm/$OBS_NAME/"
 perl Build.PL
 ./Build distmeta
 ./Build dist
-( cd $OBS_DIR ; osc rm $CPAN_NAME-*.tar.gz )
+( cd $OBS_DIR ; osc -A https://api.opensuse.org/ update ; osc rm -f $CPAN_NAME-*.tar.gz )
 cp $CPAN_NAME-*.tar.gz $OBS_DIR
 ./Build distclean
 ( cd $OBS_DIR ; osc service dr ; osc add $CPAN_NAME-*.tar.gz ; osc -A https://api.opensuse.org/ commit )
