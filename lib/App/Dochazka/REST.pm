56f7793fb405f08d4ba333857639395d20ba6829
Nathan Cutler <ncutler@suse.cz>
2015-08-06 21:58:16 +0200

    0.485
    Signed-off-by: Nathan Cutler <ncutler@suse.cz>

--- lib/App/Dochazka/REST.pm
+++ lib/App/Dochazka/REST.pm
@@ -58,11 +58,11 @@ App::Dochazka::REST - Dochazka REST server
 
 =head1 VERSION
 
-Version 0.484
+Version 0.485
 
 =cut
 
-our $VERSION = '0.484';
+our $VERSION = '0.485';
 
 
 =head2 Development status
