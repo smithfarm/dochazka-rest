ef52ab7c95e534ca6eef2056e5add01e8a5f730e
Nathan Cutler <ncutler@suse.com>
2015-12-08 16:05:23 +0100

    prerelease.sh: reverse order of commit messages in git log output
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- prerelease.sh
+++ prerelease.sh
@@ -5,7 +5,7 @@ perl Build.PL
 perl-reversion | tail -n1
 VERSION=$(grep -P 'Version \d\.*\d{3,3}' lib/App/Dochazka/REST.pm | cut -d' ' -f2)
 echo $VERSION >>Changes
-git --no-pager log $(git describe --tags --abbrev=0)..HEAD --oneline --no-color >>Changes
+git --no-pager log $(git describe --tags --abbrev=0)..HEAD --oneline --no-color --reverse >>Changes
 echo >>Changes
 git commit -as -m $VERSION
 git tag -m $VERSION $VERSION
