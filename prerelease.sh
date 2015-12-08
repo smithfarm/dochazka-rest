b517dfd619d93b9b5b3a0f378e2bbb1d97d36326
Nathan Cutler <ncutler@suse.com>
2015-12-08 16:02:56 +0100

    prerelease.sh: fix git tag syntax
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- prerelease.sh
+++ prerelease.sh
@@ -7,5 +7,5 @@ VERSION=$(grep -P 'Version \d\.*\d{3,3}' lib/App/Dochazka/REST.pm | cut -d' ' -f
 echo $VERSION >>Changes
 git --no-pager log $(git describe --tags --abbrev=0)..HEAD --oneline --no-color >>Changes
 git commit -as -m $VERSION
-git tag -m $VERSION
+git tag -m $VERSION $VERSION
 echo "Now edit Changes"
