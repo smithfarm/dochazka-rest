503a46115775b3951e868b049d504510e020c5e7
Nathan Cutler <ncutler@suse.com>
2015-12-08 16:04:21 +0100

    prerelease.sh: append newline to Changes file
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- prerelease.sh
+++ prerelease.sh
@@ -6,6 +6,7 @@ perl-reversion | tail -n1
 VERSION=$(grep -P 'Version \d\.*\d{3,3}' lib/App/Dochazka/REST.pm | cut -d' ' -f2)
 echo $VERSION >>Changes
 git --no-pager log $(git describe --tags --abbrev=0)..HEAD --oneline --no-color >>Changes
+echo >>Changes
 git commit -as -m $VERSION
 git tag -m $VERSION $VERSION
 echo "Now edit Changes"
