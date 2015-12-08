92a26194ca1132ca37fd74eb1bdf79b052702af5
Nathan Cutler <ncutler@suse.com>
2015-12-08 16:01:51 +0100

    prerelease.sh: automatically update Changes file, add tag
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- prerelease.sh
+++ prerelease.sh
@@ -3,3 +3,9 @@ perl-reversion -bump
 perl Build.PL
 ./Build distmeta
 perl-reversion | tail -n1
+VERSION=$(grep -P 'Version \d\.*\d{3,3}' lib/App/Dochazka/REST.pm | cut -d' ' -f2)
+echo $VERSION >>Changes
+git --no-pager log $(git describe --tags --abbrev=0)..HEAD --oneline --no-color >>Changes
+git commit -as -m $VERSION
+git tag -m $VERSION
+echo "Now edit Changes"
