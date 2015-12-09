efedfb5332f07e648d6b21e7df5fb67b009fd3bd
Nathan Cutler <ncutler@suse.com>
2015-12-09 13:31:12 +0100

    docker-test.sh: script to bootstrap Docker testing environment
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

---
+++ docker-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# docker-test.sh
+#
+# Start PostgreSQL in a container and then start App::Dochazka::REST
+# Dockerized testing environment in another container, linked to the
+# PostgreSQL container.
+
+docker rm -f dr >/dev/null 2>&1
+docker rm -f dr-postgres >/dev/null 2>&1
+docker run \
+    --name dr-postgres \
+    -e POSTGRES_PASSWORD=chisel \
+    -d \
+    postgres:9.3
+docker run \
+    --user smithfarm \
+    --name dr \
+    --link dr-postgres:postgres \
+    -h dr \
+    -d \
+    dochazka-rest
