0829256d068cc88c971d5044ed4389658a06087b
Nathan Cutler <ncutler@suse.com>
2016-01-02 02:32:50 +0100

    prerelease.sh: fix timestamp
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- prerelease.sh
+++ prerelease.sh
@@ -10,7 +10,7 @@ perl Build.PL
 ./Build distmeta
 perl-reversion | tail -n1
 VERSION=$(grep -P 'Version \d\.*\d{3,3}' lib/App/Dochazka/REST.pm | cut -d' ' -f2)
-echo "$VERSION $(date +'%Y-%M-%d %H:%M %Z')" >>Changes
+echo "$VERSION $(date +'%Y-%m-%d %H:%M %Z')" >>Changes
 git --no-pager log $(git describe --tags --abbrev=0)..HEAD --oneline --no-color --reverse >>Changes
 echo >>Changes
 vi Changes
