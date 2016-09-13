f4fdf4e8e90d0611bc108955abed24c6104057ea
Nathan Cutler <ncutler@suse.com>
2016-09-13 15:15:59 +0200

    tests: comment out TRAVIS_PERL_VERSION check in init routine
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- lib/App/Dochazka/REST/Test.pm
+++ lib/App/Dochazka/REST/Test.pm
@@ -145,8 +145,8 @@ sub initialize_regression_test {
 
     require App::Dochazka::REST;
 
-    my $travis_msg = "Travis CI environment detected. Skipping test.";
-    plan skip_all => $travis_msg if $ENV{'TRAVIS_PERL_VERSION'};
+    #my $travis_msg = "Travis CI environment detected. Skipping test.";
+    #plan skip_all => $travis_msg if $ENV{'TRAVIS_PERL_VERSION'};
 
     my $status = Web::MREST::init( 
         distro => 'App-Dochazka-REST', 
